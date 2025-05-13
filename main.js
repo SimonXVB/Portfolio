9dbb43308858d0511b6b45a55ace1d5d75ceb65c
SimonXVB <167611089+SimonXVB@users.noreply.github.com>
2025-05-13 18:52:45 +0200

    refactored main script

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const screen = document.querySelector("#main-screen-text");
-const form = document.querySelector("#main-text-container");
-const input = document.querySelector("#main-text-container input");
+const form = document.querySelector("#screen-input");
+const input = document.querySelector("#screen-input input");
+const pages = document.querySelectorAll(".page");
 
 document.addEventListener("DOMContentLoaded", () => {
     input.focus();
@@ -9,32 +9,65 @@ document.addEventListener("DOMContentLoaded", () => {
 form.addEventListener("submit", e => {
     e.preventDefault();
     
-    renderSwitch();
+    commandSwitch();
 
     input.value = "";
 });
 
 function clearScreen() {
-    screen.innerHTML = "";
+    pages.forEach(page => {
+        page.style.display = "none";
+    });
+
+    document.getElementById("error-container").innerHTML = "";
+};
+
+function renderPage(page) {
+    pages.forEach(page => {
+        page.style.display = "none";
+    });
+
+    document.getElementById(page).style.display = "flex";
+    document.getElementById("error-container").innerHTML = "";
+};
+
+function renderAll() {
+    pages.forEach(page => {
+        page.style.display = "flex";
+    });
 };
 
 function renderError() {
+    const errorContainer = document.getElementById("error-container");
+    const screen = document.getElementById("main-screen");
     const el = document.createElement("div");
 
     el.innerHTML = "Command not found, type 'help' for a list of commands";
     el.className = "error";
 
-    screen.appendChild(el);
+    errorContainer.appendChild(el);
 
     screen.scrollTo(0, screen.scrollHeight);
 };
 
-function renderSwitch() {
+function commandSwitch() {
     switch (input.value.toLowerCase()) {
-        case "clear": 
-            clearScreen()
+        case "home":
+            renderPage("home");
             break;
-        default : 
+        case "stack":
+            renderPage("stack");
+            break;
+        case "projects":
+            renderPage("projects");
+            break;
+        case "all": 
+            renderAll();
+            break;
+        case "clear":
+            clearScreen();
+            break;
+        default :
             renderError();
     };
 };
