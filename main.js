80078bb251044cd9b20f90f448a278a103c70b4f
SimonXVB <167611089+SimonXVB@users.noreply.github.com>
2025-06-20 18:29:43 +0200

    further improved stack page

--- main.js
+++ main.js
@@ -36,83 +36,109 @@ function imageCarousel() {
     const nextButton = document.getElementById("carousel-next");
 
     let currentIndex = 0;
-    let prevIndex = imagePanels.length - 1;
+    let prevIndex = 0;
+    let interval;
 
-    function prev() {
-        prevIndex = currentIndex;
-
-        currentIndex - 1 < 0 ? currentIndex = imagePanels.length - 1 : currentIndex--;
-
-        panelButtons[currentIndex].style.outline = "2px solid var(--main-green)";
-        panelButtons[currentIndex].scrollIntoView({ behavior: "smooth", inline: "center" });
-        panelButtons[prevIndex].style.outline = "";
-
-        imagePanels.forEach(panel => {
-            panel.style.display = "none";
-        });
-
-        imagePanels[currentIndex].style.display = "block";
-        imagePanels[currentIndex].style.animation = "transitionInLeft 1s";
-        imagePanels[currentIndex].ariaHidden = false;
-
-        imagePanels[prevIndex].style.display = "block";
-        imagePanels[prevIndex].style.animation = "transitionOutLeft 1s";
-        imagePanels[prevIndex].onanimationend = () => imagePanels[prevIndex].style.display = "none";
-        imagePanels[prevIndex].ariaHidden = true;
+    function newInterval() {
+        clearInterval(interval);
+        interval = setInterval(next, 5000);
     };
 
     function next() {
         prevIndex = currentIndex;
+        currentIndex = currentIndex + 1 > imagePanels.length - 1 ? 0 : currentIndex + 1;
 
-        currentIndex + 1 > imagePanels.length - 1 ? currentIndex = 0 : currentIndex++;
-
-        panelButtons[currentIndex].style.outline = "2px solid var(--main-green)";
-        panelButtons[currentIndex].scrollIntoView({ behavior: "smooth", inline: "center" });
-        panelButtons[prevIndex].style.outline = "";
-
-        imagePanels.forEach(panel => {
-            panel.style.display = "none";
-        });
-        
-        imagePanels[currentIndex].style.display = "block";
-        imagePanels[currentIndex].style.animation = "transitionInRight 1s";
+        imagePanels[currentIndex].scrollIntoView({behavior: "smooth", inline: "center"});
         imagePanels[currentIndex].ariaHidden = false;
-        
-        imagePanels[prevIndex].style.display = "block";
-        imagePanels[prevIndex].style.animation = "transitionOutRight 1s";
-        imagePanels[prevIndex].onanimationend = () => imagePanels[prevIndex].style.display = "none";
         imagePanels[prevIndex].ariaHidden = true;
+
+        panelButtons[prevIndex].removeAttribute("data-active");
+        panelButtons[currentIndex].setAttribute("data-active", "true");
+        panelButtons[currentIndex].scrollIntoView({behavior: "smooth", inline: "center"});
+
+        newInterval();
+    };
+
+    function prev() {
+        prevIndex = currentIndex;
+        currentIndex = currentIndex - 1 < 0 ? imagePanels.length - 1 : currentIndex - 1;
+
+        imagePanels[currentIndex].scrollIntoView({behavior: "smooth", inline: "center"});
+        imagePanels[currentIndex].ariaHidden = false;
+        imagePanels[prevIndex].ariaHidden = true;
+
+        panelButtons[prevIndex].removeAttribute("data-active");
+        panelButtons[currentIndex].setAttribute("data-active", "true");
+        panelButtons[currentIndex].scrollIntoView({behavior: "smooth", inline: "center"});
+
+        newInterval();
     };
 
     panelButtons.forEach((button, i) => {
         button.addEventListener("click", () => {
-            if(i === currentIndex) return;
-
             prevIndex = currentIndex;
             currentIndex = i;
 
-            panelButtons[currentIndex].style.outline = "2px solid var(--main-green)";
-            panelButtons[currentIndex].scrollIntoView({ behavior: "smooth", inline: "center" });
-            panelButtons[prevIndex].style.outline = "";
-
-            imagePanels.forEach(panel => {
-                panel.style.display = "none";
-            });
-
-            imagePanels[currentIndex].style.display = "block";
-            imagePanels[currentIndex].style.animation = "transitionInRight 1s";
+            imagePanels[currentIndex].scrollIntoView({behavior: "smooth", inline: "center"});
             imagePanels[currentIndex].ariaHidden = false;
-
-            imagePanels[prevIndex].style.display = "block";
-            imagePanels[prevIndex].style.animation = "transitionOutRight 1s";
-            imagePanels[prevIndex].onanimationend = () => imagePanels[prevIndex].style.display = "none";
             imagePanels[prevIndex].ariaHidden = true;
+
+            panelButtons[prevIndex].removeAttribute("data-active");
+            panelButtons[currentIndex].setAttribute("data-active", "true");
+            panelButtons[currentIndex].scrollIntoView({behavior: "smooth", inline: "center"});
+
+            newInterval();
         });
     });
 
-    prevButton.addEventListener("click", prev);
-    nextButton.addEventListener("click", next);
-    imagePanels[currentIndex].style.display = "block";
+    const swipeArea = document.getElementById("carousel-container");
+
+    let startX = 0;
+    let threshold = 50;
+    let swipeDir = "";
+    
+    swipeArea.addEventListener("touchstart", e => {
+        startX = e.touches[0].clientX;
+    });
+
+    swipeArea.addEventListener("touchmove", e => {
+        if(e.touches[0].clientX > startX + threshold) {
+            swipeDir = "right";
+        } else if(e.touches[0].clientX < startX - threshold) {
+            swipeDir = "left";
+        };
+    });
+
+    swipeArea.addEventListener("touchend", () => {
+        if(swipeDir === "left") {
+            next();
+            swipeDir = "";
+        } else if(swipeDir === "right") {
+            prev();
+            swipeDir = "";
+        } else {
+            return;
+        };
+    });
+
+    imagePanels[currentIndex].scrollIntoView();
     panelButtons[currentIndex].scrollIntoView();
+
+    nextButton.addEventListener("click", next);
+    prevButton.addEventListener("click", prev);
+
+    newInterval();
 };
 imageCarousel();
+
+function setActiveButton() {
+    const buttons = document.querySelectorAll("#buttons button");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            buttons.forEach(button => button.removeAttribute("data-active"));
+            button.setAttribute("data-active", "true");
+        });
+    });
+};
+setActiveButton();
